d9c07d00a4ced6eb42e2b5bd4d5e3a89bfc8bda2
martin-short <charles.martin425@gmail.com>
2024-12-14 15:42:20 -0700

    Delete Encapsulation directory

--- Encapsulation/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        EnhancedPlayer tim = new EnhancedPlayer("Tim", 200, "Sword");
-        System.out.println("Initial health is " + tim.healthRemaining());
-    }
-}
